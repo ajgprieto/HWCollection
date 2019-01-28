8975b2de679ca399ac26401dd1100536ea503f11
Alvin Prieto <aj.garidanprieto@gmail.com>
2019-01-27 18:39:53 -1000

    init commit

---
+++ HWCollection/src/pages/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="flex flex-center">
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      cars: [],
+      batman: [],
+      other: []
+    }
+  },
+  created: function() {
+  }
+}
+</script>
